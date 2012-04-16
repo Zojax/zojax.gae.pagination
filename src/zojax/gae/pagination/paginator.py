3b93a205c7961f2c0210e833bb2f2b2fd37063a2
Anatoly Bubenkov <bubenkoff@gmail.com>
2012-04-16 14:52:28 +0300

    tests fixed

--- src/zojax/gae/pagination/paginator.py
+++ src/zojax/gae/pagination/paginator.py
@@ -1,1 +1,122 @@
-#
+import math
+import logging
+import json
+
+from google.appengine.api import memcache
+from google.appengine.datastore.datastore_query import Cursor
+
+
+class PaginatorMixin(object):
+
+    @classmethod
+    def _pre_delete_hook(cls, key):
+        super(PaginatorMixin, cls)._pre_delete_hook(key)
+        memcache.delete(key.kind())
+
+    def _post_put_hook(self, future):
+        super(PaginatorMixin, self)._post_put_hook(future)
+        memcache.delete(self.key.kind())
+
+
+class Paginator(object):
+
+    def __init__(self, currpage, q, rows):
+        self.currpage, self.q, self.rows = currpage, q, rows
+
+
+    @classmethod
+    def get_query_id(self, query, rows):
+        return hash((str(query.filters), str(query.orders), rows))
+
+    def __call__(self):
+        #    logging.debug('Paginator started with initial vals: currpage %s, q %s, rows %s ' %(str(currpage), str(q), str(rows)))
+        FETCH_LIMIT = 1000
+        start = self.rows*(self.currpage - 1)
+        query_kind = self.q.kind
+        query_key = self.get_query_id(self.q, self.rows)
+        query_map_changed = False
+        query_map = memcache.get(query_kind)
+        if query_map is None:
+            query_map ={query_key: {}}
+        #print 'memcache cursor:' + str(cursor)
+        total_query_objects = query_map.setdefault(query_key, {}).get('total')
+        totalpages = query_map.setdefault(query_key, {}).get('pages')
+        if total_query_objects is None:
+            total_query_objects = self.q.count()
+            query_map[query_key]['total'] = total_query_objects
+            totalpages = int(math.ceil(total_query_objects/float(self.rows)))
+            query_map[query_key]['pages'] = totalpages
+            query_map_changed = True
+
+        objects = None
+        more = False
+
+        try:
+            logging.debug('trying to get objects directly from memcache')
+            objects = query_map[query_key]['cursors'][start]['objects']
+            more = query_map[query_key]['cursors'][start]['more']
+            logging.debug('got some objects!')
+        except KeyError:
+            query_map[query_key]['cursors'] = {}
+            query_map_changed = True
+
+        if objects is None:
+            try:
+                cursor = query_map[query_key]['cursors'][start]['cursor']
+            except KeyError:
+                query_map[query_key]['cursors'] = {}
+                query_map_changed = True
+                cursor = None
+
+            if cursor is not None:
+                logging.debug('cursor key found in memcache')
+                cursor = Cursor.from_websafe_string(cursor)
+            else:
+                logging.debug('cursor key not found in memcache. Start looking for the closest cursor')
+
+                cursor, diff = self.get_closest_cursor(start, query_map[query_key]['cursors'])
+                #firstly, lets set the cursor on start of page
+
+                while diff > FETCH_LIMIT:
+                    logging.debug('diff is %s moving cursor...' %(diff,))
+                    objects, cursor, more = self.q.fetch_page(FETCH_LIMIT, start_cursor=cursor, keys_only=True)
+                    query_map[query_key]['cursors'][start+diff] = dict(cursor=cursor.to_websafe_string())
+                    query_map_changed = True
+                    diff -= FETCH_LIMIT
+
+                #ok, lets set cursor more precisely
+                if diff:
+                    objects, cursor, more = self.q.fetch_page(diff, start_cursor=cursor, keys_only=True)
+                    query_map[query_key]['cursors'][start] = dict(cursor=cursor.to_websafe_string())
+                    query_map_changed = True
+            objects, cursor, more = self.q.fetch_page(self.rows, start_cursor=cursor)
+            if objects:
+                query_map[query_key]['cursors'][start+len(objects)] = dict(cursor = cursor.to_websafe_string())
+                query_map[query_key]['cursors'].setdefault(start, {'objects': objects})['objects'] = objects
+                query_map[query_key]['cursors'][start]['more'] = more
+
+        if query_map_changed:
+            memcache.set(query_kind, query_map)
+        return dict(objects=objects, totalpages=totalpages, totalrecords=total_query_objects, more=more)
+
+
+    def get_closest_cursor(self, start, cursors):
+        keys = filter(lambda x: x< start , cursors.keys())
+        if keys:
+            lower_cursor = cursors.get(max(*keys))
+        else:
+            lower_cursor = None
+        diff_list = [start]
+        if lower_cursor:
+            lower_diff = start-lower_cursor.start
+            diff_list.append(lower_diff)
+
+        diff = sorted(diff_list)[0]
+        if lower_cursor:
+            if diff == lower_diff:
+                cursor=Cursor.from_websafe_string(lower_cursor.cursor)
+        if diff == start:
+            cursor = None
+        logging.debug('the closest cursor found, diff: %s' %(str(diff)))
+
+        return cursor, diff
--- src/zojax/gae/pagination/tests/test_paginator.py
+++ src/zojax/gae/pagination/tests/test_paginator.py
@@ -1,1 +1,105 @@
-#
+from unittest import TestCase
+import os
+
+try:
+    from ndb import model
+except ImportError:
+    from google.appengine.ext.ndb import model
+
+from google.appengine.ext import testbed
+from google.appengine.api import memcache
+
+from zojax.gae.pagination.paginator import Paginator, PaginatorMixin
+
+
+class TestObject(PaginatorMixin, model.Model):
+        email_address = model.StringProperty()
+        recipients = model.StringProperty()
+        sender = model.StringProperty()
+        subject = model.StringProperty()
+
+
+class PaginateTestCase(TestCase):
+
+
+    model = TestObject
+
+    def setUp(self):
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        # Then activate the testbed, which prepares the service stubs for use.
+        self.testbed.activate()
+        # Next, declare which service stubs you want to use.
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+        self.testbed.init_mail_stub()
+        self.testbed.init_taskqueue_stub(root_path=os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+        #create message
+        self.email_address = "larry@example.com"
+        self.email_address2 = "bogdan@example.com"
+        self.TOTAL_MESSAGES = 2100
+        self.ROWS = 2
+
+
+        for i in range(self.TOTAL_MESSAGES):
+            self.model(sender = self.email_address, recipients = self.email_address + ', ' + self.email_address2,
+                subject = str(i)
+            ).put()
+        self.q = self.model.query()
+
+    def test_paginate(self):
+        current_page=10
+        result = Paginator(current_page, self.q, self.ROWS)()
+        self.assertEquals(len(result['objects']), 2)
+        self.assertEquals(result['objects'][0].subject, '18')
+        self.assertEquals(result['totalpages'], 1050)
+
+
+    def test_memcache(self):
+        current_page=10
+        result = Paginator(current_page, self.q, self.ROWS)()
+        #check whether query_key saved in memcache
+        self.assertTrue(memcache.get(self.q.kind))
+
+#        lets use memcache key to get cursor for the same query
+        result = Paginator(current_page, self.q, self.ROWS)()
+        self.assertEquals(len(result['objects']), 2)
+        self.assertEquals(result['objects'][0].subject, '18')
+
+    def test_far_page(self):
+        #use previous StoreCursor to find the closest cursor
+        current_page=8
+        Paginator(current_page, self.q, self.ROWS)()
+        current_page=10
+        result = Paginator(current_page, self.q, self.ROWS)()
+        self.assertEquals(len(result['objects']), 2)
+        self.assertEquals(result['objects'][0].subject, '18')
+        self.assertEquals(result['totalrecords'], self.TOTAL_MESSAGES)
+        self.assertEquals(result['totalpages'], 1050)
+
+        id = Paginator.get_query_id(self.q, self.ROWS)
+        self.assertEquals(len(memcache.get(self.q.kind)[id]['cursors'][18]['objects']),2)
+
+        #test same page again to get results from cache
+        result = Paginator(current_page, self.q, self.ROWS)()
+        id = Paginator.get_query_id(self.q, self.ROWS)
+        self.assertEquals(len(memcache.get(self.q.kind)[id]['cursors'][18]['objects']),2)
+
+    def test_remove_hook(self):
+        current_page=8
+        Paginator(current_page, self.q, self.ROWS)()
+        id = Paginator.get_query_id(self.q, self.ROWS)
+        self.assertEquals(len(memcache.get(self.q.kind)[id]['cursors'][14]['objects']),2)
+        # delete object
+        memcache.get(self.q.kind)[id]['cursors'][14]['objects'][0].key.delete()
+        # now memcache should be clear
+        self.assertIsNone(memcache.get(self.q.kind))
+
+
+    def test_empty_query(self):
+        result = Paginator(1, self.model.query(self.model.subject=='not existing subject'), self.ROWS)()
+        self.assertEquals(len(result['objects']), 0)
+        self.assertEquals(result['objects'], [])
+        self.assertEquals(result['totalrecords'], 0)
+        self.assertEquals(result['totalpages'], 0)
